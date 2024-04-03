42b1a0b4100cf61e0e5ac49d4658fb97841013b4
aqibsidd <aqibsid007007@gmail.com>
2024-04-03 23:07:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ require('./db/conn')
 dotenv.config({ path: './.env' });
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({ origin: "*" }));
 // Routes
